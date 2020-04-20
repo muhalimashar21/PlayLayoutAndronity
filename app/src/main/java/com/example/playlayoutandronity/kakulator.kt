d0ba3242489cc1a440e2c78fd9954e2330fd3a7d
ACER <muhalimashar21@gmail.com>
2020-04-20 17:22:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/playlayoutandronity/kakulator.kt
@@ -0,0 +1,14 @@
+package com.example.playlayoutandronity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class kakulator : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.kakulatror)
+
+    }
+
+}
